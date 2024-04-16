7ace98db10914196a02d4f29670e7af1481075ca
Narasimha12345 <lakkireddynarasimhareddy72857@gmail.com>
2024-04-16 10:07:14 +0530

    My Third Commit

---
+++ Interface.java
@@ -0,0 +1,34 @@
+public interface MyInterface {
+    int MY_CONSTANT = 42;  // Public, Static, and Final
+
+    void myMethod();  // Public and Abstract
+
+    default void defaultMethod() {  // Public and Default
+        System.out.println("Default method in interface");
+    }
+
+    static void staticMethod() {  // Public and Static
+        System.out.println("Static method in interface");
+    }
+
+    private void privateMethod() {  // Private
+        System.out.println("Private method in interface");
+    }
+}
+
+public class Main implements MyInterface {
+    @Override
+    public void myMethod() {
+        System.out.println("Implementation of myMethod");
+    }
+
+    public static void main(String[] args) {
+        Main instance = new Main();
+
+        System.out.println(MyInterface.MY_CONSTANT);  // Accessing constant
+
+        instance.myMethod();  // Accessing overridden method
+        instance.defaultMethod();  // Accessing default method
+        MyInterface.staticMethod();  // Accessing static method
+    }
+}
